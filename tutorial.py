88fd2ba870507883dbdcc50478db150f7e457cc6
Mow <mowster69@outlook.com>
2022-06-21 13:38:46 +0200

    WIP: feat : transparency

--- tutorial.py
+++ tutorial.py
@@ -1,15 +1,32 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
+# webcam interne du macbook (port 0)
+# cap = cv2.VideoCapture(0)
+# webcam externe sur le port 1
+cap = cv2.VideoCapture(1)
+
 
 while True:
     ret, frame = cap.read()
-    
-    cv2.imshow('frame', frame)
 
-    if cv2.waitKey(1) == ord('q'):
+    if not ret:
+        print('Cannot receive frame from camera')
         break
+    # frame_cam = cv2.resize(frame_cam, (width, height), interpolation = cv2.INTER_AREA)
+
+
+ # Blend the two images and show the result
+    tr = 0.3 # transparency between 0-1, show camera if 0
+    frame = ((1-tr) * frame.astype(np.float) + tr * frame.astype(np.float)).astype(np.uint8)
+    cv2.imshow('Transparent result', frame)
+    if cv2.waitKey(1) == 27: # ESC is pressed
+        break
+
+    # cv2.imshow('frame', frame)
+
+    # if cv2.waitKey(1) == ord('q'):
+    #     break
 
 cap.release()
 cv2.destroyAllWindows()
