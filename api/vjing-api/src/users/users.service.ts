acd6781c2194f7570bec9130bcdbb9187c3bd482
Mow <mowster69@outlook.com>
2022-06-24 16:38:59 +0200

    refacto(Users Service): encrypt passwords

--- api/vjing-api/src/users/users.service.ts
+++ api/vjing-api/src/users/users.service.ts
@@ -7,19 +7,42 @@ export type User = {
     password: String,
 };
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const user1Password: String = 'changeme';
+const user2Password: String = 'guess';
+
+let user1EncryptedPassword: String;
+let user2EncryptedPassword: String;
+
+bcrypt.genSalt(saltRounds, function(err, salt) {
+    bcrypt.hash(user1Password, salt, function(err, hash) {
+        // Store hash in your password DB.
+        user1EncryptedPassword = hash;
+    });
+});
+
+bcrypt.genSalt(saltRounds, function(err, salt) {
+    bcrypt.hash(user2Password, salt, function(err, hash) {
+        // Store hash in your password DB.
+        user2EncryptedPassword = hash;
+    });
+});
+
 @Injectable()
 export class UsersService {
     private user1: User = {
         userId: 1,
         username: 'john',
-        password: 'changeme',
+        password: user1EncryptedPassword,
     };
     private user2: User = {
         userId: 2,
         username: 'maria',
-        password: 'guess',
+        password: user2EncryptedPassword,
     };
-
+    
     private readonly users = [
         this.user1,
         this.user2,
