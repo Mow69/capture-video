f4a52447d371ecc26cf16826b4df8d1bc767aeeb
Mow <mowster69@outlook.com>
2022-07-04 21:31:41 +0200

    feat(patch user): add SecurityService provider in users module with fixed circular service call error

--- api/vjing-api/src/users/users.module.ts
+++ api/vjing-api/src/users/users.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { SecurityService } from 'src/tools/security.service';
 import { User } from './user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
@@ -7,7 +8,7 @@ import { UsersService } from './users.service';
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, SecurityService],
   exports: [UsersService],
 })
 export class UsersModule {}
