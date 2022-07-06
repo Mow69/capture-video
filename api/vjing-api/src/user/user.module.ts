a6a27147e2ba9ee03daa693e00ead28091b7d99a
Aurélien <aureliendu3809@outlook.fr>
2022-07-06 22:25:07 +0200

    test id exist getjson

--- api/vjing-api/src/user/user.module.ts
+++ api/vjing-api/src/user/user.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Filter } from 'src/filter/entities/filter.entity';
 import { User } from 'src/users/user.entity';
+import { UsersModule } from 'src/users/users.module';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User,Filter])],
+  imports: [TypeOrmModule.forFeature([User,Filter]),UsersModule],
   controllers: [UserController],
   providers: [UserService],
 })
--- api/vjing-api/src/user/user.service.ts
+++ api/vjing-api/src/user/user.service.ts
@@ -2,18 +2,22 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Filter } from 'src/filter/entities/filter.entity';
 import { User } from 'src/users/user.entity';
+import { UsersService } from 'src/users/users.service';
 import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
     constructor(
         @InjectRepository(User) private readonly usersRepository: Repository<User>,
-        @InjectRepository(Filter) private readonly filterRepository: Repository<Filter>
+        @InjectRepository(Filter) private readonly filterRepository: Repository<Filter>,
+        private usersService: UsersService
     ) {}
     getProfile(user){
         return user;
     }
     async userjson(userId: number){
+        await this.usersService.findOne(userId);
+        
         return await this.filterRepository.createQueryBuilder("filter")
         .leftJoinAndSelect("filter.orders", "order")
         .leftJoinAndSelect("filter.category", "category")
@@ -28,6 +32,8 @@ export class UserService {
         .execute();    
     }
     async userjsonDownloaded(userId: number){
+        await this.usersService.findOne(userId);
+
         return await this.filterRepository.createQueryBuilder("filter")
             .leftJoinAndSelect("filter.orders", "order")
             .leftJoinAndSelect("filter.category", "category")
